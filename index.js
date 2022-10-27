54abb2bdd389f370562994b11a5fc90c26d3c1e1
MaryyamSalah <maryyamslah20@gmail.com>
2022-10-27 14:00:25 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,24 @@
+
+ //devide 
+
+ const divide = (parameter1, parameter2) => {
+    console.log(`Adding ${parameter1}`);
+    console.log(`Adding ${parameter2}`);
+    return parameter1 / parameter2;
+  }
+ div(2000,100);
+
+
+//square
+let square = (x = 2  , y = 2 ) => Math.pow(x , y)
+
+
+//add
+
+let add = (x , y ) => x + y 
+
+
+
+//const add = (parameter1, parameter2) =>  parameter1 + parameter2;
+ 
+
